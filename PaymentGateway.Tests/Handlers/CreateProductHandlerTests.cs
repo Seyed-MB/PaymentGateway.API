2de9f7072ac3aa9f30f9ac129280d37530027d4f
98937 <98937@LAPTOP-3DLHA77J>
2025-10-15 22:26:01 +0330

    popline test

---
+++ PaymentGateway.Tests/Handlers/CreateProductHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using PaymentGateway.Application.Handlers;
+using PaymentGateway.Application.Models;
+
+namespace PaymentGateway.Tests.Handlers
+{
+    public class CreateProductHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ShouldInsertProductAndReturnId()
+        {
+            // Arrange
+            var inMemorySettings = new Dictionary<string, string> {
+                {"ConnectionStrings:DefaultConnection", "Server=localhost;Database=PaymentGatewayDB;User Id=sa;Password=123456;TrustServerCertificate=True;"}
+            };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            var handler = new CreateProductHandler(configuration);
+
+            var command = new CreateProductCommand
+            {
+                Name = "Test Product",
+                Price = 12345
+            };
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            Assert.True(result > 0);
+        }
+    }
+}
