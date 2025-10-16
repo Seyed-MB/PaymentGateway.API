efd68c1725cdceb3a6764294898fa5c1f2c4420f
98937 <98937@LAPTOP-3DLHA77J>
2025-10-16 16:12:59 +0330

    444

--- PaymentGateway.Tests/obj/Debug/net8.0/PaymentGateway.Tests.AssemblyInfo.cs
+++ PaymentGateway.Tests/obj/Debug/net8.0/PaymentGateway.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentGateway.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de9f7072ac3aa9f30f9ac129280d37530027d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61c3f13db6fe3a731c7751bf2acae5d737b91a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentGateway.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentGateway.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
