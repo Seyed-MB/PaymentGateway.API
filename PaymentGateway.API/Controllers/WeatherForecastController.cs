ebc5eda52da4c5ea5dae9de13f3ff82017c939e2
98937 <98937@LAPTOP-3DLHA77J>
2025-10-16 17:30:23 +0330

    333

--- PaymentGateway.API/Controllers/WeatherForecastController.cs
+++ PaymentGateway.API/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ public class ProductController : ControllerBase
     {
         var id = await _mediator.Send(command);
         Console.WriteLine("Feature branch test log");
-        return Ok(new { Message = "ÀﬁÀﬁÀﬁÀﬁÀﬁProduct created successfully4445655654", ProductId = id });
+        return Ok(new { Message = "ÀﬁÀﬁÀﬁÀﬁÀﬁProduct created3 successfully4445655654", ProductId = id });
     }
 
     [HttpGet]
