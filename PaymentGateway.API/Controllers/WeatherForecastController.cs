cf4af4b3962c63b74dd8f2a2bee3007fdd37d603
98937 <98937@LAPTOP-3DLHA77J>
2025-10-18 15:25:37 +0330

    333

--- PaymentGateway.API/Controllers/WeatherForecastController.cs
+++ PaymentGateway.API/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ public class ProductController : ControllerBase
     {
         var id = await _mediator.Send(command);
         Console.WriteLine("Feature branch test log");
-        return Ok(new { Message = "Product create successfully", ProductId = id });
+        return Ok(new { Message = "Product create successfully V1", ProductId = id });
     }
 
     [HttpGet]
--- PaymentGateway.API/obj/Debug/net8.0/PaymentGateway.API.AssemblyInfo.cs
+++ PaymentGateway.API/obj/Debug/net8.0/PaymentGateway.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentGateway.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69b79c20c0aba946bc2779c17f227bd02e6d1da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a61ceed1da8d72b4d1b89c70f3e87a21b492620")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentGateway.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentGateway.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
