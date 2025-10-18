0848ce0a433160b4ee47b24e54bf19de2e3a7c23
98937 <98937@LAPTOP-3DLHA77J>
2025-10-18 15:32:51 +0330

    41

--- PaymentGateway.API/Controllers/WeatherForecastController.cs
+++ PaymentGateway.API/Controllers/WeatherForecastController.cs
@@ -24,6 +24,7 @@ public class ProductController : ControllerBase
     }
 
     [HttpGet]
+
     public async Task<IActionResult> GetAll()
     {
         var products = await _mediator.Send(new GetAllProductsQuery());
